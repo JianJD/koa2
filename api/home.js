5c83c3b09c7119c75240932bba7a591e2076a658
jianjd <1520057670@qq.com>
2018-12-24 22:30:50 +0800

    修改上传文件的后缀名字

--- api/home.js
+++ api/home.js
@@ -39,9 +39,10 @@ router.post('/uploadfiles', async (ctx, next) => {
       // 创建可读流
       const reader = fs.createReadStream(file.path);
       // 获取上传文件扩展名
-      let date=new Date()
+      let date=new Date();
+      var FileExt = file.name.replace(/.+\./, "").toLowerCase();   //正则表达式获取后缀
       let fileName=`${date.getFullYear()}-${date.getMonth()}-${date.getDate()}-${date.getHours()}-${date.getMinutes()}-${date.getSeconds()}`
-      let filePath = path.join(__dirname, '../upload/') + `/${fileName}`;
+      let filePath = path.join(__dirname, '../upload/') + `/${fileName}.${FileExt}`;
       // 创建可写流
       const upStream = fs.createWriteStream(filePath);
       // 可读流通过管道写入可写流
--- mysqlLib/mysql.js
+++ mysqlLib/mysql.js
@@ -1,35 +1,35 @@
 var mysql = require('mysql');
 var config = require('../config/config')
 
-var pool  = mysql.createPool({
-  host     : config.database.HOST,
-  user     : config.database.USERNAME,
-  password : config.database.PASSWORD,
-  database : config.database.DATABASE,
-  port     : config.database.PORT
+var pool = mysql.createPool({
+  host: config.database.HOST,
+  user: config.database.USERNAME,
+  password: config.database.PASSWORD,
+  database: config.database.DATABASE,
+  port: config.database.PORT
 });
 // mysql连接池
-let query = ( sql, values ) => {
-    return new Promise(( resolve, reject ) => {
-      pool.getConnection( (err, connection) => {
-        if (err) {
-          reject( err )
-        } else {
-          connection.query(sql, values, ( err, rows) => {
-            if ( err ) {
-              reject( err )
-            } else {
-              resolve( rows )
-            }
-            connection.release()
-          })
-        }
-      })
+let query = (sql, values) => {
+  return new Promise((resolve, reject) => {
+    pool.getConnection((err, connection) => {
+      if (err) {
+        reject(err)
+      } else {
+        connection.query(sql, values, (err, rows) => {
+          if (err) {
+            reject(err)
+          } else {
+            resolve(rows)
+          }
+          connection.release()
+        })
+      }
     })
-  }
+  })
+}
 //   创建user表
-  let users =
-    `create table if not exists users(
+let users =
+  `create table if not exists users(
      userId INT NOT NULL AUTO_INCREMENT,
      name VARCHAR(100) NOT NULL COMMENT '用户名',
      pass VARCHAR(100) NOT NULL COMMENT '密码',
@@ -37,8 +37,8 @@ let query = ( sql, values ) => {
      moment VARCHAR(100) NOT NULL COMMENT '注册时间',
      PRIMARY KEY ( userId )
     );`
-    let article=
-        `create table if not exists article(
+let article =
+  `create table if not exists article(
             articleId INT NOT NULL AUTO_INCREMENT,
             title VARCHAR(100) NOT NULL COMMENT '文章标题',
             avator VARCHAR(100) NOT NULL COMMENT '文章作者',
@@ -46,28 +46,27 @@ let query = ( sql, values ) => {
             creatAt INT  DEFAULT  0 COMMENT '创建时间',
             PRIMARY KEY ( articleId )
         );`
-    let createTable = ( sql ) => {
-        return query( sql, [] ).then(res=>{
-            
-        }).catch(err=>{
-            console.log(err)
-        })
-      }
-      
-      // 建表
-      createTable(users)
-      createTable(article)
-      exports.addUser=function(value){
-        let _sql = "insert into users set name=?,pass=?,avator=?,moment=?;"
-        return query( _sql, value ).then(res=>{
+let createTable = (sql) => {
+  return query(sql, []).then(res => {
+  }).catch(err => {
+    console.log(err)
+  })
+}
 
-        }).catch(err=>{
+// 建表
+createTable(users)
+createTable(article)
+exports.addUser = function (value) {
+  let _sql = "insert into users set name=?,pass=?,avator=?,moment=?;"
+  return query(_sql, value).then(res => {
 
-        })
-      }
-      exports.users=function(value){
-        let _sql = "select * from users;"
-        return query( _sql, value )
+  }).catch(err => {
 
-    
-      }
+  })
+}
+exports.users = function (value) {
+  let _sql = "select * from users;"
+  return query(_sql, value)
+
+
+}
